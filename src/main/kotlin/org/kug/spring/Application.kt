b7717ef4f6350117f3af0766c5a2ce9072222ae3
Jon Schneider <jkschneider@gmail.com>
2016-05-26 15:37:42 -0700

    CGLib problem

--- src/main/kotlin/org/kug/spring/Application.kt
+++ src/main/kotlin/org/kug/spring/Application.kt
@@ -2,17 +2,23 @@ package org.kug.spring
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.DeserializationFeature
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.kotlin.getForObject
 import org.springframework.kotlin.linkEachStatic
 import org.springframework.kotlin.toPagedResources
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 open class Application {
     @Bean
     open fun restTemplate() = RestTemplate()
@@ -27,20 +33,35 @@ fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
 }
 
+@Aspect
+@Component
+class LoggingAspect {
+    @Before("execution(* org.kug.spring.QuoteRepository.*(..))")
+    fun quoteRepositoryInteraction() {
+        println("interacted with repository")
+    }
+}
+
 data class Quote(val person: String, val text: String)
 
-@RestController
-@RequestMapping("/api/quotes")
-class QuoteController {
-    val quotes = mutableSetOf(
+@Repository
+class QuoteRepository {
+    private val quotes = mutableSetOf(
             Quote("Frank Underwood", "Money is the Mc-mansion in Sarasota that starts falling apart after 10 years."),
             Quote("Frank Underwood", "Power is a lot like real estate. It’s all about location, location, location."))
 
-    @RequestMapping
     fun allQuotes() = quotes
+    fun addQuote(q: Quote) = quotes.add(q)
+}
+
+@RestController
+@RequestMapping("/api/quotes")
+class QuoteController(val repo: QuoteRepository) {
+    @RequestMapping
+    fun allQuotes() = repo.allQuotes()
 
     @RequestMapping(method = arrayOf(RequestMethod.POST))
-    fun addQuote(@RequestBody quote: Quote) = quotes.add(quote)
+    fun addQuote(@RequestBody quote: Quote) = repo.addQuote(quote)
 }
 
 data class Title(@JsonProperty("Title") val title: String, @JsonProperty("Year") val year: String)
