1927f4e69bd13631f448be2576fe5de427a5340f
Jon Schneider <jkschneider@gmail.com>
2016-05-26 13:27:48 -0700

    Rest controller

--- src/main/kotlin/org/kug/spring/Application.kt
+++ src/main/kotlin/org/kug/spring/Application.kt
@@ -2,10 +2,25 @@ package org.kug.spring
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 open class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+}
+
+data class Quote(val person: String, val text: String)
+
+@RestController
+@RequestMapping("/api/quotes")
+class QuoteController {
+    val quotes = mutableSetOf(
+            Quote("Frank Underwood", "Money is the Mc-mansion in Sarasota that starts falling apart after 10 years."),
+            Quote("Frank Underwood", "Power is a lot like real estate. It’s all about location, location, location."))
+
+    @RequestMapping
+    fun allQuotes() = quotes
 }
